e5e44e1a152a1b5253bba3206af3242486b89112
Burhan-Hasan <burhan.hasanzade@gmail.com>
2017-04-05 13:01:23 +0400

    Added base files

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # JsConvert
 NET Convert class in Js
-TEST
---
+++ convert.js
@@ -0,0 +1,9 @@
+var Convert;
+(function (Convert)
+{
+    Convert.ToDate = function (dateString, format)
+    {
+
+    }
+
+})(Convert || (Convert = {}));
