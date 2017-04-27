6431fc901ac1e73bc70cf14d0ead948c6f480993
Burhan <burhan.hasan.dev@gmail.com>
2017-04-27 23:07:24 +0400

    Added functions StartDateOfWeek & EndDateOfWeek

--- helper.js
+++ helper.js
@@ -14,6 +14,43 @@ var DateHelper;
         return result;
     }
 
+    DateHelper.StartDateOfWeek = function () {
+        var _1_jan = new Date(year, 0, 1);
+
+        // no calculation needed
+        if (week == 1)
+            return _1_jan;
+
+        var _1_jan_day_of_week = (_1_jan.getUTCDate() + 5) % 7 + 1;
+        var tmpDate = new Date(((week - 1) * 7 + 1 - _1_jan_day_of_week) * 86400000 + _1_jan.getTime());
+
+        var year = tmpDate.getFullYear(),
+            month = tmpDate.getMonth(),
+            day = tmpDate.getDate(),
+            dayOfWeek = tmpDate.getDay();
+
+        if (dayOfWeek != 1)
+            return new Date(year, month, day - dayOfWeek + 1);
+        return tmpDate;
+    }
+
+    DateHelper.EndDateOfWeek = function () {
+        var tmpDate = startDateOfWeek(week, year)
+
+        var year = tmpDate.getFullYear(),
+            month = tmpDate.getMonth(),
+            day = tmpDate.getDate(),
+            dayOfWeek = tmpDate.getDay();
+
+        if (dayOfWeek != 0) {
+            tmpDate = new Date(year, month, day + 7 - dayOfWeek);
+            if (tmpDate.getFullYear() != year)
+                tmpDate = new Date(year, 11, 31);
+        }
+
+        return tmpDate;
+    }
+
     function dayOfYear(d) {
         var start = new Date(d.getFullYear(), 0, 0);
         var diff = d - start;
