a2f49b64bcf7320a0da547c16469aacbab41d44e
Burhan <burhan.hasan.dev@gmail.com>
2017-06-08 19:07:10 +0400

    Minor bug fix

--- convert.js
+++ convert.js
@@ -1,35 +1,34 @@
 var Convert;
-(function (Convert)
-{
+(function (Convert) {
     //***PUBLIC
-    Convert.ToDate = function (dateString, format)
-    {
+    Convert.ToDate = function (dateString, format) {
         var dtSegments = getDateSegments(dateString.trim(), format);
         var date = new Date();
         date.setYear(dtSegments.year);
         date.setMonth(dtSegments.month);
         date.setDate(dtSegments.day);
+        date.setMilliseconds(0);
+        date.setSeconds(0);
+        date.setMinutes(0);
+        date.setHours(0);
 
         return date;
     }
-    Convert.DateToString = function (date, format)
-    {
-        if (!validateDate(date))
-        {
+    Convert.DateToString = function (date, format) {
+        if (!validateDate(date)) {
             console.error('Invalid Date');
             return;
         }
 
         var seperators = getSeperators(format);
-        if (seperators.length > 1)
-        {
+        if (seperators.length > 1) {
             console.error('More than one separator was found.');
             return;
         }
 
         var year = date.getFullYear(),
-        month = ('0' + (date.getMonth() + 1)).slice(-2),
-        day = ('0' + date.getDate()).slice(-2);
+            month = ('0' + (date.getMonth() + 1)).slice(-2),
+            day = ('0' + date.getDate()).slice(-2);
 
         return getStringFromDateSegments({
             year: year,
@@ -39,14 +38,13 @@ var Convert;
     }
 
     //***PRIVATE
-    function getDateSegments(dateString, format)
-    {
+    function getDateSegments(dateString, format) {
         var splittedFormat = null,
             splittedDate = null;
 
         var separate = null;
         if (~dateString.indexOf('-')) separate = '-'
-        else if (~dateString.indexOf('.'))  separate = '.'
+        else if (~dateString.indexOf('.')) separate = '.'
 
         splittedDate = dateString.split(separate);
         splittedFormat = format.split(separate);
@@ -59,26 +57,22 @@ var Convert;
         return dt;
     }
 
-    function getStringFromDateSegments(dateSegments, format)
-    {
+    function getStringFromDateSegments(dateSegments, format) {
         return format.replace(/dd/, dateSegments.day).replace(/MM/, dateSegments.month).replace(/yyyy/, dateSegments.year);
     }
 
-    function validateDate(date)
-    {
+    function validateDate(date) {
         if (
             Object.prototype.toString.call(date) === '[object Date]' &&
             date.toString !== 'Invalid Date'
-           )
+        )
             return true;
         return false;
     }
 
-    function getSeperators(format)
-    {
+    function getSeperators(format) {
         var allSeperators = {};
-        for (var i = 0; i < format.length; i++)
-        {
+        for (var i = 0; i < format.length; i++) {
             var charCode = format[i].charCodeAt(0);
             if (charCode < 65 || charCode > 122)
                 allSeperators[charCode] = format[i];
@@ -86,8 +80,7 @@ var Convert;
         return Object.values(allSeperators);
     }
 
-    function convertShortYearToFull(year)
-    {
+    function convertShortYearToFull(year) {
         if (year.toString().length > 2) return;
         year = +year;
 
@@ -97,7 +90,7 @@ var Convert;
         else
             prefix = "20";
 
-        return +(prefix + ('0'+year).slice(-2));
+        return +(prefix + ('0' + year).slice(-2));
     }
 
 })(Convert || (Convert = {}));
